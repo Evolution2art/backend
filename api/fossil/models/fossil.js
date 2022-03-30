1f87c172c3cb72af07d16d70bd2dbc96abcfdd47
Evolution2Art <luc@2enhance.co>
2022-03-30 14:43:17 +0000

    Add Mail Templates single content type Add component Mail to use as fields in templates Add field replacement options, eg. {fossil.name}

--- api/fossil/models/fossil.js
+++ api/fossil/models/fossil.js
@@ -1,8 +1,10 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  url: "https://testje",
+};
