b57f24be81c6ba64a1c3cfc2121b303cc874f7cf
Evolution2Art <luc@2enhance.co>
2022-04-21 15:02:55 +0000

    add default /admin subpath to strapi server config

--- config/admin.js
+++ config/admin.js
@@ -9,7 +9,11 @@ module.exports = ({ env }) => ({
     "STRAPI_ADMIN_URL",
     env(
       "STRAPI_URL",
-      "http://" + env("HOST", "0.0.0.0") + ":" + env.int("PORT", 1338)
+      "http://" +
+        env("HOST", "0.0.0.0") +
+        ":" +
+        env.int("PORT", 1337) +
+        "/admin"
     )
   ),
 });
