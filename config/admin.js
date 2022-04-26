29ffdfe1681aa20714e8cdf177f4af0a6b16e5bd
Evolution2Art <luc@2enhance.co>
2022-04-26 14:17:37 +0000

    Upgrade strapi to latest version

--- config/admin.js
+++ config/admin.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   apiToken: {
-    salt: env("API_TOKEN_SALT", "0b7af7c8a258414c99bd92f9d967fcf5"),
+    salt: env("API_TOKEN_SALT"),
   },
   auth: {
-    secret: env("ADMIN_JWT_SECRET", "7186d7f6-82d9-48c0-902c-a9b433d3a45a"),
+    secret: env("ADMIN_JWT_SECRET"),
   },
   url: env(
     "STRAPI_ADMIN_URL",
--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   app: {
-    keys: [env("ADMIN_JWT_SECRET", "3d410da9-e1ba-4341-9b31-ca485071781f")],
+    keys: [env("APP_KEY")],
   },
   url: env(
     "STRAPI_URL",
